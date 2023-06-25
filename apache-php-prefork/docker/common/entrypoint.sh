8f3d1e952b11bf00578d4a375b9199b7e573a6ef
1noro <ppuubblliicc@protonmail.com>
2023-06-25 18:41:56 +0200

    feat(apache-prefork): mejorando la config de apache

--- apache-php-prefork/docker/common/entrypoint.sh
+++ apache-php-prefork/docker/common/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "[begin] entrypoint.sh"
-test -f /1noro/entrypoint_extension.sh && \
-    source /1noro/entrypoint_extension.sh
+test -f /inoro/entrypoint_extension.sh && \
+    source /inoro/entrypoint_extension.sh
 echo "[ end ] entrypoint.sh"
 echo "[start] runnning original entrypoint: /usr/local/bin/docker-php-entrypoint" "$@"
 exec /usr/local/bin/docker-php-entrypoint "$@"
--- apache-php-prefork/docker/prod/entrypoint_extension.sh
+++ apache-php-prefork/docker/prod/entrypoint_extension.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "[begin] entrypoint_extension.sh"
+echo "[begin] entrypoint_extension.sh (production)"
 
-source /1noro/common/common_entrypoint_extension.sh
+source /inoro/common/common_entrypoint_extension.sh
 
 echo "[ end ] entrypoint_extension.sh"
